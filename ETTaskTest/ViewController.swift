ccad08fab2c0dfdfc9ad8f414589147ba23ff001
MailwanLab29 <mailwanlab@gmail.com>
2017-12-29 19:51:59 +0530

    Initial Commit

---
+++ ETTaskTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ETTaskTest
+//
+//  Created by Daya Ram Mailwan on 12/29/17.
+//  Copyright © 2017 MailwanLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
